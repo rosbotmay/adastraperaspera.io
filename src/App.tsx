5246378f8a88aa5291afbefd69285210f634fbb9
Rosa Mayila <128403959+rosbotmay@users.noreply.github.com>
2025-12-10 11:51:26 +0100

    Fix github pages workflow

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -11,14 +12,20 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
+      {/* 👇 IMPORTANT: basename for GitHub Pages project site */}
+      <BrowserRouter basename="/adastraperaspera.io">
         <Routes>
           <Route path="/" element={<Index />} />
+
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* <Route path="/something" element={<Something />} /> */}
+
           <Route path="*" element={<NotFound />} />
         </Routes>
+
+        {/* Toasts */}
+        <Toaster />
+        <Sonner />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById("root") as HTMLElement;
+
+createRoot(container).render(<App />);
